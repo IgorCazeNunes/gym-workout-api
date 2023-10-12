3cab8a474d03f129b195ebb375dfe02a8f0e8e36
Crummorn <igorcaze@gmail.com>
2023-10-12 06:41:34 -0300

    chore(typeorm): config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExercisesModule } from './modules/exercises/exercises.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './ormconfig';
 
 @Module({
-  imports: [ExercisesModule],
+  imports: [ExercisesModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
