0154eb3ad39154efe5d440a691c2525469501fa2
Crummorn <igorcaze@gmail.com>
2023-10-13 21:39:01 -0300

    feat(exercise): add relation with series

--- src/modules/exercises/entities/exercise.entity.ts
+++ src/modules/exercises/entities/exercise.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Serie } from 'src/modules/series/entities/series.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Exercise {
@@ -10,4 +11,7 @@ export class Exercise {
 
   @Column()
   image: string;
+
+  @OneToMany(() => Serie, (serie) => serie.exercise)
+  series: Serie[];
 }
