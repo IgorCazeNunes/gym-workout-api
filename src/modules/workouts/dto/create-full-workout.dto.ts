2da8b3461e23256c13f34ef5a0b97cb31884625e
Crummorn <igorcaze@gmail.com>
2023-10-15 13:04:57 -0300

    chore(workout): add class validator

--- src/modules/workouts/dto/create-full-workout.dto.ts
+++ src/modules/workouts/dto/create-full-workout.dto.ts
@@ -1,19 +1,44 @@
+import {
+  IsString,
+  IsDate,
+  ValidateNested,
+  ArrayMinSize,
+  IsArray,
+  IsUUID,
+  IsNumber,
+} from 'class-validator';
+
 class ExerciseDto {
+  @IsUUID()
   id: string;
 }
 
 class RepDto {
+  @IsNumber()
   kg: number;
+
+  @IsNumber()
   qty: number;
 }
 
 class SerieDto {
   exercise: ExerciseDto;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
   reps: RepDto[];
 }
 
 export class CreateFullWorkoutDto {
+  @IsString()
   name: string;
+
+  @IsDate()
   finish_at: Date;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
   series: SerieDto[];
 }
