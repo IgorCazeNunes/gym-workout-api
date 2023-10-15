f7b1f40f25adbe113b9dbd62934c98632a6413c9
Crummorn <igorcaze@gmail.com>
2023-10-15 20:55:00 -0300

    feat(series): adjust dto validation

--- src/modules/series/dto/create-series.dto.ts
+++ src/modules/series/dto/create-series.dto.ts
@@ -1,4 +1,25 @@
+import { Type } from 'class-transformer';
+import { IsObject, IsOptional, IsUUID, ValidateNested } from 'class-validator';
+
+class ExerciseDto {
+  @IsUUID()
+  id: string;
+}
+
+class WorkoutDto {
+  @IsUUID()
+  id: string;
+}
+
 export class CreateSeriesDto {
-  exercise: { id: string };
-  workout: { id: string };
+  @IsObject()
+  @ValidateNested()
+  @IsOptional()
+  @Type(() => ExerciseDto)
+  exercise: ExerciseDto;
+
+  @IsObject()
+  @ValidateNested()
+  @Type(() => WorkoutDto)
+  workout: WorkoutDto;
 }
