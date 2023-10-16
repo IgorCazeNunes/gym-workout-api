27ee8da0ba1541a17ccef9342fa0cfea12d2f517
Crummorn <igorcaze@gmail.com>
2023-10-15 21:01:25 -0300

    feat(exercises): adjust dto validation

--- src/modules/exercises/dto/create-exercise.dto.ts
+++ src/modules/exercises/dto/create-exercise.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class CreateExerciseDto {
+  @IsString()
   name: string;
+
+  @IsString()
   image: string;
 }
--- src/modules/exercises/dto/update-exercise.dto.ts
+++ src/modules/exercises/dto/update-exercise.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateExerciseDto } from './create-exercise.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateExerciseDto extends PartialType(CreateExerciseDto) {}
+export class UpdateExerciseDto extends PartialType(CreateExerciseDto) {
+  @IsOptional()
+  name: string;
+
+  @IsOptional()
+  image: string;
+}
