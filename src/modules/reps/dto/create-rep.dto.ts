7235d057a63776a70f5ac3f667e936bade6bc44b
Crummorn <igorcaze@gmail.com>
2023-10-15 21:00:30 -0300

    feat(reps): adjust dto validation

--- src/modules/reps/dto/create-rep.dto.ts
+++ src/modules/reps/dto/create-rep.dto.ts
@@ -1,6 +1,32 @@
+import { Type } from 'class-transformer';
+import {
+  IsNumber,
+  IsObject,
+  IsPositive,
+  IsString,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+
+class SerieDto {
+  @IsUUID()
+  id: string;
+}
+
 export class CreateRepDto {
+  @IsNumber()
+  @IsPositive()
   kg: number;
+
+  @IsNumber()
+  @IsPositive()
   qty: number;
+
+  @IsString()
   type: string;
-  serie: { id: string };
+
+  @IsObject()
+  @ValidateNested()
+  @Type(() => SerieDto)
+  serie: SerieDto;
 }
--- src/modules/reps/dto/update-rep.dto.ts
+++ src/modules/reps/dto/update-rep.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRepDto } from './create-rep.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateRepDto extends PartialType(CreateRepDto) {}
+export class UpdateRepDto extends PartialType(CreateRepDto) {
+  @IsOptional()
+  kg: number;
+
+  @IsOptional()
+  qty: number;
+
+  @IsOptional()
+  type: string;
+}
